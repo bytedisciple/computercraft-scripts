f0dfc6f07f1d5dfe7c98c1cbf57a21df561d6603
bytedisciple <46268272+bytedisciple@users.noreply.github.com>
2022-07-26 16:09:01 -0400

    Update bees.lua

--- bees.lua
+++ bees.lua
@@ -1,1 +1,35 @@
+turtleslots = 14
+
+
+//Await input that breeding is complete
+
+
+// Break and replace the breeder
 turtle.select(1)
+turtle.dig("right")
+turtle.place()
+
+//Wait a moment for items to return to turtles inv
+os.sleep(1)
+
+//If we have seeds, place them
+for i=1, turtleslots, +1
+do
+    local item = turtle.ItemDetails(i)
+    if item then
+      print(item.name)
+      if item.name == "Seeds" then
+        turtle.select(i)
+        turtle.drop()
+        break
+    end
+end
+  
+shouldContinue = true
+    
+while shouldContinue do
+      local indexBestChicken = redstone.getAnalogInput("bottom")
+      print(indexBestChicken)
+      turtle.select(indexBestChicken)
+      shouldContinue = turtle.drop()
+end

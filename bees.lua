dc3b246a4c7f8312c15b97eb4bdade9f980238cb
bytedisciple <46268272+bytedisciple@users.noreply.github.com>
2022-07-26 17:13:29 -0400

    Update bees.lua

--- bees.lua
+++ bees.lua
@@ -4,67 +4,77 @@ turtleslots = 16
 --Await input that breeding is complete
 
 
--- Break and replace the breeder
-turtle.dig("right")
-
-placed = false
-for i=1, turtleslots
-do
-    local item = turtle.getItemDetail(i)
-    if item then
-      print(item.name)
-      if item.name == "roost:breeder" then
-        turtle.select(i)
-        placed = turtle.place()
-        break
-      end
-    end
-end
-
-if ~placed then
-    print("cannot find breeder!")
-    os.exit(1)
-end
-
-
---Wait a moment for items to return to turtles inv
-os.sleep(1)
-
---If we have seeds, place them
-for i=1, turtleslots
-do
-    print(i)
-    local item = turtle.getItemDetail(i)
-    if item then
-      print(item.name)
-      if item.name == "minecraft:wheat_seeds" then
-        turtle.select(i)
-        turtle.drop()
-        break
-      end
-    end
-end
-  
-shouldContinue = true
+while true do
     
-while shouldContinue do
-      local indexBestChicken = redstone.getAnalogInput("back")
-      print(indexBestChicken)
-      turtle.select(indexBestChicken)
-      t = turtle.drop()
-      print(t)
-      shouldContinue = t
-      os.sleep(1)
-end
+    --Wait for current round to be finished breeding
+    while redstone.getAnalogInput("right") == 0 do
+        sleep(1)
+    then
+    
+    -- Break and replace the breeder
+    turtle.dig("right")
 
-for i=1, turtleslots
-do
-    local item = turtle.getItemDetail(i)
-    if item then
-      if item.name ~= "minecraft:wheat_seeds" and item.name ~= "roost:breeder" then
-        turtle.select(i)
-        turtle.dropDown()
-        break
-      end
+    placed = false
+    for i=1, turtleslots
+    do
+        local item = turtle.getItemDetail(i)
+        if item then
+          print(item.name)
+          if item.name == "roost:breeder" then
+            turtle.select(i)
+            placed = turtle.place()
+            break
+          end
+        end
+    end
+
+    if ~placed then
+        print("cannot find breeder!")
+        os.exit(1)
+    end
+
+
+    --Wait a moment for items to return to turtles inv
+    os.sleep(1)
+
+    --If we have seeds, place them
+    for i=1, turtleslots
+    do
+        print(i)
+        local item = turtle.getItemDetail(i)
+        if item then
+          print(item.name)
+          if item.name == "minecraft:wheat_seeds" then
+            turtle.select(i)
+            turtle.drop()
+            break
+          end
+        end
+    end
+
+    shouldContinue = true
+
+    --Put best chickens up for breeding
+    while shouldContinue do
+          local indexBestChicken = redstone.getAnalogInput("back")
+          print(indexBestChicken)
+          turtle.select(indexBestChicken)
+          t = turtle.drop()
+          print(t)
+          shouldContinue = t
+          os.sleep(1)
+    end
+
+    --Drop all extras below
+    for i=1, turtleslots
+    do
+        local item = turtle.getItemDetail(i)
+        if item then
+          if item.name ~= "minecraft:wheat_seeds" and item.name ~= "roost:breeder" then
+            turtle.select(i)
+            turtle.dropDown()
+            break
+          end
+        end
     end
 end
